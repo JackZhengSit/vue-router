896c8002d5f14b5a2696dcea6b7f7b587758b85f
Jack Zheng <JackZhengSit@outlook.com>
2020-09-14 16:39:06 +0800

    first

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/brazil">Brazil</router-link>
+      <router-link to="/panama">Panama</router-link>
+      <router-link to="/hawaii">Hawaii</router-link>
+      <router-link to="/jamaica">Jamaica</router-link>
       <router-link to="/about">About</router-link>
-    </div>
+    </div>-->
+    <TheNavigation />
     <router-view />
   </div>
 </template>
-
+<script>
+import TheNavigation from "@/components/TheNavigation";
+export default {
+  components: {
+    TheNavigation
+  }
+};
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -15,18 +27,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
   },
   {
     path: "/about",
@@ -17,14 +17,45 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/brazil",
+    name: "Brazil",
+    component: () => import(/* webpackChunkName: "brazil" */ "../views/Brazil"),
+  },
+  {
+    path: "/panama",
+    name: "Panama",
+    component: () => import(/* webpackChunkName: "panama" */ "../views/Panama"),
+  },
+
+  {
+    path: "/hawaii",
+    name: "Hawaii",
+    component: () => import(/* webpackChunkName: "hawaii" */ "../views/Hawaii"),
+  },
+
+  {
+    path: "/jamaica",
+    name: "Jamaica",
+    component: () =>
+      import(/* webpackChunkName: "jamaica" */ "../views/Jamaica"),
+  },
+  {
+    path: "/details/:id",
+    name: "DestinationDetails",
+    component: () =>
+      import(
+        /*webpackChunkName:"DestinationDetails" */ "../views/DestinationDetails"
+      ),
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
