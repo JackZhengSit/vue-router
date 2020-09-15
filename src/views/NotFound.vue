49922f935249f3c4e54db6d0f76bf07919941598
Jack Zheng <JackZhengSit@outlook.com>
2020-09-15 16:39:46 +0800

    end

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Not Found</h1>
+    <p>
+      Oopa we couldn't find that paage. Try going
+      <router-link :to="{name:'Home'}">Home</router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/views/User.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Welcome {{user}}</h1>
+    <p>
+      <router-link :to="{
+            name:'Invoices'
+        }">Inovices</router-link>
+    </p>
+    <button @click="logout" class="btn">Log out</button>
+  </div>
+</template>
+
+<script>
+import store from "@/store/store.js";
+export default {
+  data() {
+    return {
+      user: store.user
+    };
+  },
+  methods: {
+    logout() {
+      store.user = null;
+      this.$router.push("/");
+    }
+  }
+};
+</script>
+
+<style>
+</style>
